9e8472ad08a239dbdd399ae32e162bb0688e51d3
Jhomiguel Florian <jhomiguelflorianmendez20@gmail.com>
2022-10-23 18:37:29 -0400

    feat: enable api version

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,14 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.NODE_PORT || 4000;
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.HEADER,
+    header: 'api-version',
+  });
   await app.listen(PORT, () =>
     console.log(`Server is running on port: ${PORT}`),
   );
