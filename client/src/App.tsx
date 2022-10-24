bb0a5a93aac43a1997a4bd50d0df1849cf01a75f
Jhomiguel Florian <jhomiguelflorianmendez20@gmail.com>
2022-10-24 14:50:27 -0400

    feat: override query client default options

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,26 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 0,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      retry: false,
+      onError: (error) => console.log(error),
+    },
+  },
+});
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>hello world</div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
