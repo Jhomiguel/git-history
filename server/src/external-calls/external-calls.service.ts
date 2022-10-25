f57cf16e66d47b7851f0828a0613d13230b4b329
Jhomiguel Florian <jhomiguelflorianmendez20@gmail.com>
2022-10-25 15:12:11 -0400

    refactor: remove unused log on server app

--- server/src/external-calls/external-calls.service.ts
+++ server/src/external-calls/external-calls.service.ts
@@ -1,10 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { OctokitService } from 'nestjs-octokit';
-import { ConfigService } from '@nestjs/config';
-import * as _ from 'lodash';
-import { UtilService } from 'src/utils/app-util.service';
-import { GetCommitParamsDto } from 'src/commit-history/dto/get-commit-params.dto';
-import { GetCommitHistoryDto } from 'src/commit-history/dto/get-commit-history.dto';
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { OctokitService } from "nestjs-octokit";
+import { ConfigService } from "@nestjs/config";
+import * as _ from "lodash";
+import { UtilService } from "src/utils/app-util.service";
+import { GetCommitParamsDto } from "src/commit-history/dto/get-commit-params.dto";
+import { GetCommitHistoryDto } from "src/commit-history/dto/get-commit-history.dto";
 
 @Injectable()
 export class ExternalCallsService {
@@ -19,21 +19,20 @@ export class ExternalCallsService {
   ): Promise<GetCommitHistoryDto[]> {
     try {
       const { data: response } = await this.octokitService.request(
-        'GET /repos/{owner}/{repo}/commits',
+        "GET /repos/{owner}/{repo}/commits",
         {
-          owner: this.configService.get<string>('GITHUB_REPO_OWNER'),
-          repo: this.configService.get<string>('GITHUB_REPO_NAME'),
+          owner: this.configService.get<string>("GITHUB_REPO_OWNER"),
+          repo: this.configService.get<string>("GITHUB_REPO_NAME"),
           per_page: params.perPage,
           ...params,
         },
       );
 
-      console.log(response);
-      const commits = response.map((data) => _.pick(data, ['commit', 'sha']));
+      const commits = response.map((data) => _.pick(data, ["commit", "sha"]));
       return this.utilService.fromSnakeToCamel(commits);
     } catch (e) {
       throw new BadRequestException(
-        'An error occurred retrieving list of commits. \n Params: ',
+        "An error occurred retrieving list of commits. \n Params: ",
         JSON.stringify(params),
       );
     }
